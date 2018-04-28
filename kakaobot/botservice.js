e68ff09c7426cb6f040b024b3d93fbc2d57b2b02
Glitch (kakao-bot-templage) <none>
2018-04-28 04:36:03 +0000

    🏃💰 Checkpoint ./server.js:602196/2821 ./app.js:602196/1175 ./package.json:602196/1162 ./bot.js:602196/1970 ./botservice.js:602196/876 ./message.js:602196/1363 ./routes/bot.js:602196/20 ./kakaobot/botservice.js:602196/10

---
+++ kakaobot/botservice.js
@@ -0,0 +1,37 @@
+/**
+ * Created by cheese on 2017. 2. 9..
+ */
+
+const
+  request = require('request'),
+  cheerio = require('cheerio'),
+  async = require('async'),
+  message = require('../kakaobot/message'),
+  Bot = {};
+
+//const getApiai = require('../service/getApiai');
+//const cache = require('memory-cache');
+
+Bot.choseMenu = (req, content, callback) => {
+
+  switch (content) {
+    case "왕좌의 게임은 무엇인가?":
+      callback(null, message.baseType("왕좌의 게임은 미국드라마입니다."));
+      break;
+
+    case "왕좌의 게임에서 다음에 죽을 사람은?":
+      callback(null, message.baseType("당신입니다."));
+      break;
+
+    default:
+      callback(null, message.baseType("알 수 없는 명령입니다."));
+      /*
+      getApiai(content, function (data) {
+        callback(null, message.baseTypeText(data));
+      });
+      */
+      break;
+  }
+};
+
+module.exports = Bot;
--- server.js
+++ server.js
@@ -1,22 +1,91 @@
-// server.js
-// where your node app starts
+#!/usr/bin/env node
 
-// init project
-var express = require('express');
-var app = express();
+/**
+ * Module dependencies.
+ */
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+let app = require('./app');
+let debug = require('debug')('botnode:server');
+let http = require('http');
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+/**
+ * Get port from environment and store in Express.
+ */
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+let port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+let server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  let bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  let addr = server.address();
+  let bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
