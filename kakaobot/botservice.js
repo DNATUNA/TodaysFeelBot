fc704721e5ed82d723bc03544678e4c7ddfe14a7
Glitch (kakao-bot-templage) <none>
2018-05-07 13:49:49 +0000

    🏊🏼📷 Checkpoint ./kakaobot/message.js:602196/12 ./kakaobot/botservice.js:602196/524

--- kakaobot/botservice.js
+++ kakaobot/botservice.js
@@ -20,7 +20,8 @@ Bot.choseMenu = (req, content, callback) => {
       break;
 
     case "왕좌의 게임에서 다음에 죽을 사람은?":
-      callback(null, message.baseType("당신입니다."));
+      //callback(null, message.baseType("당신입니다."));
+      callback(null, message.photoType("다음에 죽을 사람은?", "https://s-i.huffpost.com/gen/5643294/thumbs/o-THE-570.jpg?7", "스노우 입니다.", "https://www.huffingtonpost.kr/2018/01/04/story_n_18936424.html") );
       break;
 
     default:
